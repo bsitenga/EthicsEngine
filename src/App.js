862e3ce9ac2b2534a59879da2bdc28b8426d1618
bsitenga <bsitenga@gmail.com>
2020-10-24 18:37:11 -0400

    added navbar skeleton

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import Home from './components/Home.js';
 import Quiz from './components/Quiz.js';
 import About from './components/About.js';
 import Methodology from './components/Methodology.js';
 import Error from './components/Error.js'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
   return (
     <main>
+      <Navbar bg="light" variant="light">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="quiz">Quiz</Nav.Link>
+          <Nav.Link href="about">About</Nav.Link>
+          <Nav.Link href="methodology">Methodology</Nav.Link>
+        </Nav>
+      </Navbar>
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/quiz" component={Quiz} />
