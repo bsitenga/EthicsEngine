11bce6f3dfda5c9ee78b4124c079f9c344d0f241
bsitenga <bsitenga@gmail.com>
2020-10-24 21:07:28 -0400

    styled home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -8,13 +9,14 @@ function Home() {
             <Container>
                 <Row>
                     <Col lg={8} >
-                        <h1>Reframing the way autonomous vehicles are designed</h1>
+                        <h1>Shape the way autonomous vehicles are designed</h1>
                         <h6>Ethics Engine is a platform designed to measure public opinion of
-                            the ethical challenges presented by automous driving</h6>
+                            the moral challenges presented by automous driving.</h6>
                         <p>The quiz will show you a series of moral dilemmas involving an 
                             autonomous vehicle. You will choose how the autonomous vehicle 
                             should act in each situation. Afterwards, you'll be shown your
                             responses compared to the average of all others.</p>
+                        <Link to="/quiz"><button>Start Quiz</button></Link>
                     </Col>
                     <Col lg={4} >Column 2</Col>
                 </Row>
