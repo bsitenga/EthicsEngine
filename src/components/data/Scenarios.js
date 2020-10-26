b1b771fd40182f135a373affacc750173d45b3be
bsitenga <bsitenga@gmail.com>
2020-10-26 14:49:23 -0400

    added scenarios

--- src/components/data/Scenarios.js
+++ src/components/data/Scenarios.js
@@ -1,11 +1,11 @@
 module.exports = [
-    { Left: "Hit three pedestrians in your way", Right: "Swerve into one pedestrian"},
-    { Left: "Hit five pedestrians in your way", Right: "Swerve into one pedestrian" },
-    { Left: "Hit three pedestrians", Right: "Swerve into a barricade, killing all passengers" },
+    { Left: "Hit three pedestrians in the car's way", Right: "Swerve into one pedestrian"},
+    { Left: "Hit three pedestrians in the car's way", Right: "Swerve into a barricade, killing all passengers" },
     { Left: "Hit a barricade, killing all passengers", Right: "Swerve into the adjacent lane, with unknown consequences" },
-    { Left: "Hit three pedestrians", Right: "Swerve into the adjacent lane, with unkown consequences" },
-    { Left: "", Right: "" },
-    { Left: "", Right: "" },
-    { Left: "", Right: "" },
-    { Left: "", Right: "" },
-    { Left: "", Right: "" }];
+    { Left: "Hit ten pedestrians in the car's way", Right: "Swerve into one pedestrian" },
+    { Left: "Hit three pedestrians in the car's way", Right: "Swerve into the adjacent lane, with unkown consequences" },
+    { Left: "Hit a barricade, killing all passengers", Right: "Swerve into five pedestrians" },
+    { Left: "Hit an unidentified object, with unknown consequences", Right: "Swerve into a pedestrian" },
+    { Left: "Hit three pedestrians in the car's way", Right: "Swerve into three pedestrians" },
+    { Left: "Hit an unidentified object, with unknown consequences", Right: "Swerve into the adjacent lane, with unknown consequences" },
+    { Left: "Hit a barricade, killing all passengers", Right: "Swerve into an unidentified object, with unknown consequences" }];
