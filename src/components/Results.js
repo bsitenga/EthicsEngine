9ef6d530928a913337f23ac9157ee7d73eb990b0
bsitenga <bsitenga@gmail.com>
2020-11-04 22:31:08 -0500

    added totals

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios'
 import Chart from 'chart.js';
+import Totals from './data/Totals';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -22,21 +23,22 @@ class Results extends React.Component {
   componentDidMount() {
     axios.get('http://localhost:5000/summary')
       .then(res => {
-        console.log(res);
+        //Fetch Summary Data
         this.setState({ aUtil: res.util });
         this.setState({ aAction: res.action });
         this.setState({ aKnown: res.known });
         this.setState({ aPedestrians: res.pedestrians });
         this.setState({ fetched: true })
-        const myChartRef = this.chartRef.current.getContext("2d");
 
+        //Create Radar Chart
+        const myChartRef = this.chartRef.current.getContext("2d");
         new Chart(myChartRef, {
           type: "radar",
           data: {
             labels: ["Saving More", "Saving Less", "Action", "Inaction", "Known", "Unknown", "Pedestrians", "Passengers"],
             datasets: [
               {
-                label: "Average",
+                label: "You",
                 data: [.7, .6, .3, .3, .6, .9, .5, .5],
                 backgroundColor: 'rgb(255, 103, 135, .3)',
                 borderColor: 'rgb(255, 103, 135)',
@@ -44,8 +46,8 @@ class Results extends React.Component {
                 pointBorderColor: '#FFFFFF'
               },
               {
-                label: "You",
-                data: [.9, .4, .6, .5, .3, .8, .4, .6],
+                label: "Average",
+                data: [.4, .4, .6, .5, .3, .8, .4, .6],
                 backgroundColor: 'rgb(54, 162, 235, .3)',
                 borderColor: 'rgb(54, 162, 235)',
                 pointBackgroundColor: 'rgb(54, 162, 235)',
@@ -63,6 +65,14 @@ class Results extends React.Component {
           }
         });
       })
+  }
+
+  parseData() {
+    let util1 = 0;
+    
+    return {
+      
+    }
   }
 
   render() {
---
+++ src/components/data/Totals.js
@@ -0,0 +1,10 @@
+module.exports = {
+    util1: 6,
+    util2: -6,
+    action1: -20,
+    action2: 20,
+    known1: 8,
+    known2: -8,
+    pedestrians1: 6,
+    pedestrians2: -6
+};
